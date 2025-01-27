c8afd1592531b012e41fe3a8b09e798205e7f412
Sindhu C <sindhuc@mediguru.io>
2025-01-28 02:19:03 +0530

    added terraform files

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,6 +1,7 @@
 organization_name = "Genworx Organization"
+org_name          = "Genworx Organization"
 parent_id         = "r-1p5x"  # Replace with your actual root ID
-user_emails = {
+users_email = {
   "dev"      = "dev_user@example.com"
   "prod"     = "prod_user@example.com"
   "security" = "security_user@example.com"
--- variable.tf
+++ variable.tf
@@ -3,12 +3,17 @@ variable "org_name" {
   type        = string
 }
 
+variable "organization_name" {
+  description = "The name of the organization."
+  type        = string
+}
+
 variable "parent_id" {
   description = "The parent ID for the organizational units."
   type        = string
 }
 
 variable "users_email" {
-  description = "A list of email addresses for creating IAM users."
-  type        = list(string)
+  description = "A map of email addresses for creating IAM users."
+  type        = map(string)
 }
